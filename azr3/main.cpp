701ec838ba4f528e23a6ce5e81d340a5d5b67836
Lars Luthman <lars.luthman@gmail.com>
2007-12-18 11:19:30 +0000

    Added copyright notice to main.cpp

--- azr3/main.cpp
+++ azr3/main.cpp
@@ -1,3 +1,24 @@
+/****************************************************************************
+    
+    AZR-3 - An organ synth
+    
+    Copyright (C) 2006-2007 Lars Luthman <lars.luthman@gmail.com>
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as published
+    by the Free Software Foundation.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 01222-1307  USA
+
+****************************************************************************/
+
 #include <cstring>
 #include <fstream>
 #include <iostream>
